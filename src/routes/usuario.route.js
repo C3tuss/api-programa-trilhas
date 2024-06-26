4c22354aaaa741be5262044298deb75a07f05fe8
C3tuss <alexandrevilela.sds@gmail.com>
2024-06-25 23:32:41 -0300

    substituindo U por u

--- src/routes/usuario.route.js
+++ src/routes/usuario.route.js
@@ -2,10 +2,10 @@ import express from "express";
 import UsuarioController from "../controllers/usuarios.controller.js";
 
 const routes = express.Router();
-routes.post("/Usuario", UsuarioController.cadastrarUsuario);
-routes.get("/Usuario", UsuarioController.listarUsuarios);
-routes.get("/Usuario/:id", UsuarioController.listarUsuarioPorId);
-routes.put("/Usuario/:id", UsuarioController.atualizarUsuario);
-routes.delete("/Usuario/:id", UsuarioController.excluirUsuario);
+routes.post("/usuario", UsuarioController.cadastrarUsuario);
+routes.get("/usuario", UsuarioController.listarUsuarios);
+routes.get("/usuario/:id", UsuarioController.listarUsuarioPorId);
+routes.put("/usuario/:id", UsuarioController.atualizarUsuario);
+routes.delete("/usuario/:id", UsuarioController.excluirUsuario);
 
 export default routes;
